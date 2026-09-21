5cf6ace289ca304e6eb775f25570e1ee68a1525a
TechTpvr <farrokhzadpvr@gmail.com>
2026-09-21 18:41:00 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,9 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
 android {
     namespace = "com.nabz.news"
     compileSdk = 35
@@ -18,4 +24,37 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+}
+
+dependencies {
+    implementation(platform("androidx.compose:compose-bom:2025.01.00"))
+
+    implementation("androidx.activity:activity-compose:1.10.1")
+
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    implementation("androidx.compose.material3:material3:1.3.1")
+    implementation("androidx.compose.material:material-icons-extended")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+
+    implementation("androidx.work:work-runtime-ktx:2.10.0")
+
+    implementation("androidx.core:core-ktx:1.15.0")
+
+    implementation("androidx.browser:browser:1.8.0")
+
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+
+    implementation("io.coil-kt:coil-compose:2.7.0")
+
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+    implementation("org.jsoup:jsoup:1.18.3")
+
+    implementation("com.google.code.gson:gson:2.12.1")
+
+    implementation("com.google.mlkit:translate:17.0.3")
 }
